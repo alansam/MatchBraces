473da4e60073907d5c39d29000cf399ef8936b6c
Alan Sampson <alansam@users.noreply.github.com>
2022-12-18 22:32:47 +0000

    Rename test files to .h to prevent automatic compilation.

--- test_good.h
+++ test_good.h
@@ -4,4 +4,4 @@ int main(int argc, char const * argv[]) {
   int RC = EXIT_SUCCESS;
   return RC;
 }
-#endif /
+#endif /* DO_NOT_COMPILE */
--- test_gronk.h
+++ test_gronk.h
@@ -1,4 +1,6 @@
+#ifdef DO_NOT_COMPILE
 //  MARK: main()
 int main(int argc, char const * argv[]) {
   int RC = EXIT_SUCCESS;
   return RC;
+#endif /* DO_NOT_COMPILE */
